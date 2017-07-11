d09f6938b2d3f43f774aad7d897e224badb759d5
GaelPellissier <gael.pellissier@gmail.com>
2017-07-11 15:15:12 +0200

    Initial commit

---
+++ src/ch/hevs/dataExtractor/Main.java
@@ -0,0 +1,16 @@
+package ch.hevs.dataExtractor;
+
+import ch.hevs.dataExtractor.saveSystem.MySQLExtractor;
+
+import java.util.Scanner;
+
+/**
+ * Created by gael on 07.07.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        MySQLExtractor ex = new MySQLExtractor();
+
+    }
+}
---
+++ src/ch/hevs/dataExtractor/saveSystem/MySQLExtractor.java
@@ -0,0 +1,13 @@
+package ch.hevs.dataExtractor.saveSystem;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.*;
+
+/**
+ * Created by gael on 07.07.17.
+ */
+public class MySQLExtractor {
+    private Connection conn;
+
+}
